c0d674402a9dc9437f15751b8e9435232f7312c2
Alberto Gómez Peña <albertogomp@gmail.com>
2018-10-04 10:08:29 +0200

    arreglo en comentario p5417.java

--- TEMA 5/p5417.java
+++ TEMA 5/p5417.java
@@ -1,8 +1,8 @@
 public class p5417 { 
   public static void main(String[] args){
     System.out.print("\n \n \n \n \n \n POR FAVOR, INSERTE EL NUMERO:  ");
-    long num = Integer.parseInt(System.console().readLine());
-    long sum = 0;
+    long num = Integer.parseInt(System.console().readLine()); 
+    long sum = 0; //ALMACENA LA SUMA
     if(num<=0){
       System.out.println("EL NUMERO INTRODUCIDO NO ES VALIDO");
     } else{
@@ -10,6 +10,6 @@ public class p5417 {
           sum += cont;
         }
         System.out.println("LA SUMA TOTAL ES "+sum);
-      }
+      } //DEBIDO A MI ESTILO DE CODIGO SIEMPRE EMPLEO INT CONT/CONT2/CONT3/... PARA USAR CONTADORES EN LOS BUCLES
   }
 }
