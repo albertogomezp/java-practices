5a42893fd68c491a6749d8ff18686daeccf36b2d
Alberto Gómez Peña <albertogomp@gmail.com>
2018-10-09 09:30:00 +0200

    p5427.java

---
+++ TEMA 5/p5427.java
@@ -0,0 +1,15 @@
+public class p5427 { 
+  public static void main(String[] args){
+    //--------------------------iNTRODUCCION DE DATOS----------------------
+    System.out.print(" POR FAVOR, INSERTE UN NUMERO:  ");         //INTRODUCE EL NUMERO
+    int num = Integer.parseInt(System.console().readLine()); //SE ALMACENA EL NUMERO
+    int cont2 = 1; //CONTADOR QUE IRA CALCULANDO LOS MULTIPLOS DE 3
+    for (int cont = 1; cont<=(num/3); cont++){ //AL SER TODOS MULTIPLOS DE 3, EL CONTADOR1 SERA IGUAL AL 1/3 DE NUM CUNADO CONT2=NUM
+      System.out.println(cont2*3); //PRINT
+      cont2++; //AUMENTA EN 1 EL CONT2
+    }
+
+
+
+  }
+}
