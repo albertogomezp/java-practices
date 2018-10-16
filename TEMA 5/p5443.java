08ea6d1e67572986c27b3aee9837a8bdc5dcfedf
Alberto Gómez Peña <albertogomp@gmail.com>
2018-10-16 11:09:46 +0200

    P5443 inacabado

---
+++ TEMA 5/p5443.java
@@ -0,0 +1,15 @@
+public class p5443{  //------------------------- INACABADO ---------------------------------
+  public static void main(String[] args){
+  System.out.print("INTRODUZCA UN NUMERO ");
+  String numero = System.console().readLine();
+  int longi = numero.length();
+  int num = Integer.parseInt( numero );
+  System.out.print("INTRODUZCA LA POSICION POR LA QUE QUIERE PARTIR EL NUMERO ");
+  int div = Integer.parseInt(System.console().readLine());
+  System.out.print("LOS NUMEROS PARTIDOS SON ");
+  for (int )
+
+
+
+  }
+}
