aaed45212f2c7447b011b56a75cfd73135b0ecdb
Alberto Gómez Peña <albertogomp@gmail.com>
2018-10-16 09:31:23 +0200

    Varios (arreglo p5428 y p5439.java)

--- TEMA 5/p5439.java
+++ TEMA 5/p5439.java
@@ -1,12 +1,11 @@
-public class p5428 { 
+public class p5439 { 
   public static void main(String[] args){
     System.out.print(" POR FAVOR, INSERTE UN NUMERO (al que quiere calcular el factorial):  ");         //INTRODUCE EL NUMERO
     long num = Long.parseLong(System.console().readLine()); //SE ALMACENA EL NUMERO
     long ans= 1;
     for (int cont = 1 ; cont <=num ; cont++){
       ans=cont*ans;
+      System.out.println(cont+"! = "+ans);
     }
-    System.out.println(num+"! = "+ans);
-
   }
 }
