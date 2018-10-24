30a16d138bf9628a712992f9c925d87a7c1c6810
Alberto Gómez Peña <albertogomp@gmail.com>
2018-10-24 13:56:03 +0200

    p637 (modificado) y p638

--- TEMA 6/p638.java
+++ TEMA 6/p638.java
@@ -1,26 +1,26 @@
 //Escribe un programa que muestre tres apuestas de la quiniela en tres columnas para los 14 partidos y el pleno al quince (15 filas).
-public class p637{ 
+public class p638{ 
   public static void main(String[] args){
-    int Match = 0;
     String Result ="";
     String adjust ="";
     int victory = 0;
+    int Prob = 0;
     for(int bet = 1 ; bet <= 3 ; bet++){
       System.out.print("              APUESTA #"+bet+"            ");
     }
     System.out.println("");
     for (int bet2 =  1; bet2 <= 15; bet2++){
       for (int cont = 1; cont <=3; cont++){
-        victory = (int)((Math.random()*3)+1);
-        if (victory == 1){
+         Prob = (int)((Math.random()*6)+1);
+         if(Prob <= 3){
           Result ="1";
-        }
-        if (victory == 2){
-          Result ="2";
-        }
-        if (victory == 3){
+         }
+         if(Prob >3 && Prob<=5){
           Result ="x";
-        }
+         }
+         if(Prob == 6){
+          Result ="2";
+         }
         if(bet2 <10){
           adjust="0";
         }
