b4e837f48d59ab73a264f9d7e3835d2b7bea09ed
Alberto Gómez Peña <albertogomp@gmail.com>
2018-11-03 00:36:26 +0100

    Update TEMA 7/p746.java

--- TEMA 7/p746.java
+++ TEMA 7/p746.java
@@ -3,18 +3,22 @@ public class p746{
     int [] Request =  new int[15];
     int [] Adjust = new int[15];
     System.out.println("Please, insert 15 numbers ");
-    for (int AskNumbers = 0; AskNumbers <= Request.length-1; AskNumbers++){
+    // --> Numbers request
+    for (int AskNumbers = 0; AskNumbers <= Request.length-1; AskNumbers++){ 
       System.out.print("Request["+AskNumbers+"] = ");
       Request[AskNumbers] = Integer.parseInt(System.console().readLine());
     }
+    //--> setting the arrays 
     for (int AdjustNumbers = 0; AdjustNumbers <= Request.length-2; AdjustNumbers++){
       Adjust[(AdjustNumbers+1)] = Request[AdjustNumbers];
     }
+    //--> Print
     Adjust[0] = Request[Request.length-1];
     for (int ShowNumbers = 0; ShowNumbers<=Request.length-1; ShowNumbers++){
       System.out.println("Request["+ShowNumbers+"] = "+Adjust[ShowNumbers]);
     }
-
+    //In fact, we are able to do this using just one array
+    //En realidad, esto se puede usar empleando solo un array
 
 
   }
