4f06b919b750a0661d59dd18bea6e904d85c5281
Alberto Gómez Peña <albertogomp@gmail.com>
2018-11-06 09:13:17 +0100

    Update TEMA 7/p743.java

--- TEMA 7/p743.java
+++ TEMA 7/p743.java
@@ -1,13 +1,1 @@
-public class p743{ 
-  public static void main(String[] args){
-    int [] Inverter = new int [10];
-    for( int AskNumber = 0; AskNumber <Inverter.length; AskNumber++){
-      System.out.print("Please, introduce a number: ("+AskNumber+"/10) ");
-      Inverter[AskNumber] = Integer.parseInt(System.console().readLine());
-    }
-    for (int ShowNumbers = Inverter.length-1; ShowNumbers>=0; ShowNumbers--){
-      System.out.println("RequestedNumber["+ShowNumbers+"] = "+Inverter[ShowNumbers]);
-    }
-   
-  }
-}
+Error
