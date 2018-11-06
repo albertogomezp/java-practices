886607cb1cfc9d3a612166deb47c38f166b44c38
Alberto Gómez Peña <albertogomp@gmail.com>
2018-11-06 08:44:48 +0100

    p747.java

--- TEMA 7/p747.java
+++ TEMA 7/p747.java
@@ -1,16 +1,26 @@
-// Escribe un programa que genere 100 números aleatorios del 0 al 20 y que los muestre por pantalla separados por espacios.
-// El programa pedirá entonces por teclado dos valores y a continuación cambiará todas las ocurrencias del primer valor por 
-// el segundo en la lista generada anteriormente. Los números que se han cambiado deben aparecer entrecomillados.
 public class p747{ 
   public static void main(String[] args){
     int [] Numbers =  new int[100];
+    //---> Generating numbers
     for (int GenerateNumbers = 0; GenerateNumbers <Numbers.length-1; GenerateNumbers++){
       Numbers[GenerateNumbers] = (int)(Math.random()*21);
       System.out.print(Numbers[GenerateNumbers]+" ");
-   }
-   System.out.print("\n Please, insert a value: ");
-   int FirstValue = Integer.parseInt(System.console().readLine());
-   System.out.print(" Please, another value: ");
-   int SecondValue = Integer.parseInt(System.console().readLine());
+    }
+    //---> Asking for the Wanted Number and also the number we are going to replace for it
+    System.out.print("\n Please, insert a number: ");
+    int WantedNumber = Integer.parseInt(System.console().readLine());
+    System.out.print(" Please, the new number: ");
+    int Replacer = Integer.parseInt(System.console().readLine());
+    //--> Replace and print
+    System.out.println("Looking for "+WantedNumber+" and replace for "+Replacer);
+    for (int ReplaceNumbers = 0 ; ReplaceNumbers <= Numbers.length-1 ; ReplaceNumbers++){
+      if(Numbers[ReplaceNumbers] == WantedNumber){ //---> If this numbers == the number we are looking for..
+        Numbers[ReplaceNumbers] = Replacer; //---> Replace the number
+        System.out.print("["+Numbers[ReplaceNumbers]+"] "); //---> print and adding [ ] in order to show the replaced numbers 
+      }
+      else{
+        System.out.print(Numbers[ReplaceNumbers]+" "); //---> print
+      }
+    }
   }
 }
