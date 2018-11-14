b2a3bc2e95b40e7fb4d67186ef790299817f76e7
Alberto Gómez Peña <albertogomp@gmail.com>
2018-11-14 12:23:58 +0100

    p752.java

--- TEMA 7/src/p752.java
+++ TEMA 7/src/p752.java
@@ -6,48 +6,46 @@ import java.util.Scanner;
 *     TEMA 7 PRACTICA p752       *
 *                                *
 *********************************/
-
 public class p752{
   public static void main(String[] args ) throws InterruptedException{
+    //-->Set-up
     int [][] Tables = new int[4][5];
     int PrintAdjust;
     int RelativeAdd = 0;
     int RelativeAdd2 = 0;
     int TotalAdd = 0;
+    //-->Request
     Scanner scanner = new Scanner(System.in);
     System.out.println("Insert 20 (int) numbers");
-    for(int ArrowRequest = 0; ArrowRequest <4; ArrowRequest++){
-      for(int ColumnRequest = 0; ColumnRequest <5; ColumnRequest++){
-        Tables[ArrowRequest][ColumnRequest] =(int)(Math.random()*20); // scanner.nextInt();    
+    for(int columnRequest = 0; columnRequest <4; columnRequest++){
+      for(int RowRequest = 0; RowRequest <5; RowRequest++){
+        Tables[columnRequest][RowRequest] =scanner.nextInt(); //--> Inserting into the array    
       }
-      //System.out.println("");
     }
-    System.out.println(" _________________________________________________________");
-    
+    System.out.println(" _________________________________________________________");  //--> Cuteness
     System.out.printf(" |%6s|  |%6s|  |%6s|  |%6s|  |%6s| ||%5s|\n", " C  0 "," C  1 "," C  2 "," C  3 "," C  4 ","ADD ");
     System.out.println(" ---------------------------------------------------------");
-    for(int ArrowPrint = 0; ArrowPrint <4; ArrowPrint++){
-      for(int ColumPrint = 0; ColumPrint <5; ColumPrint++){
-        PrintAdjust = Tables[ArrowPrint][ColumPrint];
-        RelativeAdd += PrintAdjust;
-        TotalAdd +=PrintAdjust;
-        System.out.printf(" |%5d%1s| ", PrintAdjust," ");
+    //-->Printing and also adding (just horizontaly)
+    for(int columnPrint = 0; columnPrint <4; columnPrint++){
+      for(int RowPrint = 0; RowPrint <5; RowPrint++){
+        PrintAdjust = Tables[columnPrint][RowPrint];
+        RelativeAdd += PrintAdjust; //--> For the relative add
+        TotalAdd +=PrintAdjust; //-->
+        System.out.printf(" |%5d%1s| ", PrintAdjust," "); //-> printing
       }
       System.out.printf("||%5d|\n", RelativeAdd);
-      RelativeAdd = 0;
+      RelativeAdd = 0; //-->reset
     }
-
-    System.out.println(" |------|--|------|--|------|--|------|--|------|-||-----|");
-    for(int ColumPrint2 = 0; ColumPrint2 <5; ColumPrint2++){
-      for(int ArrowPrint2 = 0; ArrowPrint2 <4; ArrowPrint2++){
-        PrintAdjust = Tables[ArrowPrint2][ColumPrint2];
+    System.out.println(" |------|--|------|--|------|--|------|--|------|-||-----|");  //--> Cuteness
+    for(int RowPrint2 = 0; RowPrint2 <5; RowPrint2++){ //--> Check and add the vertical add
+      for(int columnPrint2 = 0; columnPrint2 <4; columnPrint2++){
+        PrintAdjust = Tables[columnPrint2][RowPrint2];
         RelativeAdd2 += PrintAdjust;
-        TotalAdd +=PrintAdjust;
       }
-      System.out.printf(" |%5d%1s| ", RelativeAdd2," ");
+      System.out.printf(" |%5d%1s| ", RelativeAdd2," "); //--> print the add
       RelativeAdd2 = 0;
     }
-    System.out.printf("||%5d|\n", TotalAdd);
+    System.out.printf("||%5d|\n", TotalAdd); //--> total add
     System.out.println(" ---------------------------------------------------------");
   }
 }
