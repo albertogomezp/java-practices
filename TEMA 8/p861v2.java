8fcff2ad9c540d6ec91a46101553d014b588d5ed
Alberto Gómez Peña <albertogomp@gmail.com>
2018-11-28 13:48:58 +0100

    adjust

--- TEMA 8/p861v2.java
+++
@@ -1,7 +0,0 @@
-public class p861{
-  public static void ContLetras(long x) {
-    String cadena;
-    System.out.println("Introduzca la cadena de caracteres a contar: ");
-    cadena = System.console().readLine();
-  }
-}
