c5248477d7b11ad624e4da45791a57ba79e6f09c
Alberto Gómez Peña <albertogomp@gmail.com>
2018-11-30 12:23:09 +0100

    p8615.java

---
+++ TEMA 8/p8615.java
@@ -0,0 +1,11 @@
+public class p8615{ 
+  public static void main(String[] args) {
+	long num = 1;
+	while (num <=1000) {
+		num = mats.operaciones.SiguientePrimo(num);
+		System.out.println(" "+num+" ");
+	}
+	  
+	  
+  }
+}
