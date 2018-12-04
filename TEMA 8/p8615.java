d2ed7ae29b6e51bc9d8cb0a5387bf880282f376c
Alberto Gómez Peña <albertogomp@gmail.com>
2018-12-04 08:57:14 +0100

    adjust

--- TEMA 8/p8615.java
+++ TEMA 8/p8615.java
@@ -1,11 +1,10 @@
+import mats.operaciones;
 public class p8615{ 
   public static void main(String[] args) {
 	long num = 1;
 	while (num <=1000) {
 		num = mats.operaciones.SiguientePrimo(num);
 		System.out.println(" "+num+" ");
-	}
-	  
-	  
+	}  
   }
 }
