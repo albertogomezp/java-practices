82a607f94715bec59988c23ce091383bf2c0aee0
Alberto Gómez Peña <albertogomp@gmail.com>
2018-12-04 08:57:50 +0100

    p8618.java

---
+++ TEMA 8/p8616.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import mats.operaciones;
+public class p8616{
+	  public static void main(String[] args) {
+		String NumberInserted ="";
+		int NumberLength = 0;
+	      Scanner EsCap = new Scanner(System.in);
+		for (int cont = 1; cont<=99999; cont++) {
+			 NumberInserted = Integer.toString(cont);
+		      char[] Numbers = NumberInserted.toCharArray();
+		      NumberLength = NumberInserted.length();
+		      if(mats.operaciones.EsCapicua(NumberInserted, Numbers,NumberLength) == true) {
+			      System.out.println(" "+cont);
+		      }
+		}
+	}
+}
