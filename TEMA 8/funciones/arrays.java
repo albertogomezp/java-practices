1a86a051a339691c005f856f6a4c3eb835380866
Alberto Gómez Peña <albertogomp@gmail.com>
2018-12-11 11:15:19 +0100

    p8621.java

--- TEMA 8/funciones/arrays.java
+++ TEMA 8/funciones/arrays.java
@@ -19,6 +19,38 @@ public class arrays{
     }
     return ArAns;
   } 
+  /**
+  *  Devuelve el mínimo del array que se pasa como parámetro.
+  * <p>
+  * Por ejemplo, <code>generaArrayInt(100, 10, 30)</code> devolvería un array
+  * de 100 números generados al azar comprendidos entre 10 y 30.
+  * 
+  * @param Ar     Array
+  * @return      Devuelve el menor numero encontrado en <code>Ar[]</code>
+  */
+	public static int minimoArrayInt(int[] Ar) {
+    int MinNumber = 1000;
+    // ---> Asking for numbers and comparing 
+    for( int Search = 0; Search <Ar.length; Search++){
+      if (Ar[Search] < MinNumber){ // ---> In case this number is the Minimun (relative)
+        MinNumber = Ar[Search];
+      }
+    }
+    return MinNumber;
+  } 
+  public static int maximoArrayInt(int[] Ar) {
+    int MaxNumber = 0;
+    // ---> Asking for numbers and comparing 
+    for( int Search = 0; Search <Ar.length; Search++){
+      if (Ar[Search] > MaxNumber){ // ---> In case this number is the Maximun (relative)
+        MaxNumber = Ar[Search];
+       
+      }
+    }
+    return MaxNumber;
+  } 
+  
+  
   
   /**
   * Para imprimir arrays
