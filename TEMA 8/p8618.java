81bfd8d856d49d6a220d30701a47561b87b3de54
Alberto Gómez Peña <albertogomp@gmail.com>
2018-12-11 09:14:07 +0100

    Update p8618.java

--- TEMA 8/p8618.java
+++ TEMA 8/p8618.java
@@ -1,30 +1,21 @@
+/*
+* Conversor binario - decimal
+*
+* @author Alberto Gómez Peña - 1º DAW
+*
+*/
+
+
 import java.util.Scanner;
 import conversores.funciones;
-public class p8617{
-  public static void main(String[] args) {
-    Scanner Number = new Scanner(System.in);
-    System.out.println("Introduzca el numero");
-	String Introduccion = Number.nextLine();
-
-    Scanner Base = new Scanner(System.in);
-    System.out.println("Indique la base (1.- Binario \n2.- Decimal \n3.- Hexagesimal \n4.- Octal");
-	int base = BinEs.nextLine();
-
-    String Solucion ="";
-    switch(base){
-        case 1: //binaria
+public class p8618{
+    public static void main(String[] args) {
+        Scanner Number = new Scanner(System.in);
+        System.out.println("Introduzca el numero binario");
+        String Introduccion = Number.nextLine();
+        String Solucion ="";
         Scanner NumberBin = new Scanner(System.in);
         Solucion = conversores.funciones.BinBase(Introduccion);
-        break;
-        case 2: //Decimal
-        break;
-        case 3: //Hexagesimal
-        case 4: //octal
-        break;
-        default:
-        System.out.println("ERROR");
-        break;
-    }
 		System.out.println(Solucion);
 	}
 }
