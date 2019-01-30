f6717a70f3e0e44e14728fb0e5213a7154e735e3
Alberto Gómez Peña <albertogomp@gmail.com>
2019-01-30 10:56:52 +0100

    Create Gestor.java

---
+++ TEMA 9/Arrays de objetos/Ejercicio1/Gestor.java
@@ -0,0 +1,17 @@
+public class Gestor{
+  public static void main(String[] args) {
+    System.out.println("Vamos a crear un array de gatitos!");
+    Gato[] Argatos = new Gato[4];
+    int edad = 0;
+    String nombre = "";
+    for(int x = 0; x<4; x++){
+      System.out.println("Introduzca el nombre del gato");
+      nombre = System.console().readLine();
+      Argatos[x].setNombreGato(nombre);
+      System.out.println("Introduzca la edad del gato");
+      edad = Integer.parseInt(System.console().readLine());
+      Argatos[x].setEdad(edad);
+      System.out.println(Argatos[x]);   
+    } 
+  }
+}
