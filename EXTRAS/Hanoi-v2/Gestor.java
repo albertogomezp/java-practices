f7acbf7a424514e18c8367abd02a1022b1ee2a18
Alberto Gómez Peña <albertogomp@gmail.com>
2019-01-31 08:04:31 +0100

    update (beta)

--- EXTRAS/Hanoi-v2/Gestor.java
+++ EXTRAS/Hanoi-v2/Gestor.java
@@ -4,9 +4,58 @@
 */
 public class Gestor {
   public static void main(String[] args) {
+
+    int origen = 0;
+    int destino = 0;
+    boolean ganar = false;
+    int aros;
+    int ancho;
+    int numerotorres;
     System.out.println("Indique el numero de aros: ");
-    int aros = Integer.parseInt(System.console().readLine());
-    int ancho = (aros*2)+1;
+    aros = Integer.parseInt(System.console().readLine());
+    ancho = (aros*2)+1;
+    String[] rellenar2 = new String[aros+1];
+    rellenar2=rellenoTorres(aros);
+    // Inicializa el tablero
+
+
+    System.out.println("Indique el numero de Torres: ");
+    numerotorres = Integer.parseInt(System.console().readLine());
+    Torre[] torres = new Torre[numerotorres];
+    for(int x = 0; x < numerotorres; x++){
+      torres[x] = new Torre(aros);
+    }
+    
+    //Mete los aros en la primera torre
+    for(int cont = 0; cont <aros; cont++){
+      torres[0].setRelleno(aros-cont,cont);
+      torres[0].setAlturaRelativa(cont+1);
+    }   
+    //Bucle
+    while (origen !=-1 && destino !=-1 && ganar == false){
+      for(int x = aros-1; x>=0; x--){
+        for(int parte = 0; parte <numerotorres; parte++){
+          System.out.print(rellenar2[torres[parte].Relleno[x]]);
+        }
+        System.out.println(" ");
+      }
+      System.out.println("Indique Aro de Origen (-1 para salir)");
+      origen = Integer.parseInt(System.console().readLine());
+      System.out.println("Indique Aro de Destino (-1 para salir)");
+      destino = Integer.parseInt(System.console().readLine());
+      torres[origen-1].moverAro(torres[destino-1]);      
+      ganar = checkHanoi(torres, numerotorres, aros, ganar);
+      
+      
+      
+    }  
+    
+  }
+  /**
+   * Genera los aros para la primera torre
+   * @param aros El numero de aros que tendrá nuestro juego
+   */
+  public static String[] rellenoTorres(int aros){
     String[] rellenar = new String[aros+1];
     String[] rellenar2 = new String[aros+1];
     String centro = "|";
@@ -22,49 +71,30 @@ public class Gestor {
       //System.out.println(rellenar[cont]);
       relleno="";
     }
-    for(int x = 0; x<=aros; x++){
-      rellenar2[x] = rellenar[aros-x];
-    }
-    // Inicializa el tablero
-    System.out.println("Indique el numero de Torres: ");
-    int numerotorres = Integer.parseInt(System.console().readLine());
-    Torre[] torres = new Torre[numerotorres];
-    for(int x = 0; x < numerotorres; x++){
-      torres[x] = new Torre(aros);
-    }
-    //Mete los aros en la primera torre
-    for(int cont = 0; cont <aros; cont++){
-      torres[0].setRelleno(aros-cont,cont);
-      torres[0].setAlturaRelativa(cont+1);
-    }
     
     
-    //Pre-bucle
-    int origen = 0;
-    int destino = 0;
-    boolean ganar = false;
-    //Bucle
-    while (origen !=-1 && destino !=-1 && ganar == false){
-      for(int x = aros-1; x>=0; x--){
-        for(int parte = 0; parte <numerotorres; parte++){
-          System.out.print(rellenar2[torres[parte].Relleno[x]]);
-        }
-        System.out.println(" ");
-      }
-      System.out.println("Indique Aro de Origen (-1 para salir)");
-      origen = Integer.parseInt(System.console().readLine());
-      System.out.println("Indique Aro de Destino (-1 para salir)");
-      destino = Integer.parseInt(System.console().readLine());
-      torres[origen-1].moverAro(torres[destino-1]);      
-      if(torres[numerotorres-1].getAlturaRelativa()==aros){
-        ganar = true;
-        System.out.println("VICTORIA MAGISTRAL");
-      }
-
-
-
-
-    }  
-    
+    for(int cont = 0; cont <=aros; cont++){
+      rellenar2[cont]=rellenar[aros-cont];
+    }
+    return rellenar2;
   }
+  /**
+   * Comrpueba si se ha ganado
+   * @param torres array de torres
+   * @param numerotorres cuantas torres hay
+   * @param aros cuantos aros hay
+   * @param ganar
+   * @return ganar si se cumple la condicion
+   */
+  public static boolean checkHanoi(Torre[] torres, int numerotorres, int aros, boolean ganar){
+    if(torres[numerotorres-1].getAlturaRelativa()==aros){
+      ganar = true;
+      System.out.println("VICTORIA MAGISTRAL");
+    }
+    return ganar;
+  }
+  
+  
+  
+  
 }
