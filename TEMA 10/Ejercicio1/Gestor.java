412c1822560ae1f2b77c50956abae09441984f6f
Alberto Gómez Peña <albertogomp@gmail.com>
2019-02-05 08:51:50 +0100

    Ejercicio 1

---
+++ TEMA 10/Ejercicio1/Gestor.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Gestor{
+  public static void main(String[] args) {
+    ArrayList<String> Nombres = new ArrayList<String>();
+    System.out.println("Introduce el nombre de 6 compas de clase");
+    for(int x = 0; x <6; x++){
+      System.out.println("Nombre "+(x+1)+"/6: ");
+      Nombres.add(System.console().readLine());
+    }
+    for(String num : Nombres){
+      System.out.println(num);
+    }
+  }
+}
